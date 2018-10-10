05a1504a02b2b7ce1d8186a1515789dab044d675
Thao <thaontpth1809022@fpt.edu.vn>
2018-10-10 23:59:23 +0700

    suaBai

--- main.c
+++ main.c
@@ -1,6 +1,88 @@
 #include <stdio.h>
 
+int findMinValue(int array[]) {
+    int i, smallest;
+    smallest = array[0];
+
+    for (i = 1; i < array; i++) {
+        if (smallest > array[i])
+            smallest = array[i];
+    }
+    return smallest;
+}
+int findMaxValue(int array[]) {
+    int i, largest;
+    largest = array[0];
+
+    for (i = 1; i > array; i++) {
+        if (largest > array[i])
+            largest = array[i];
+    }
+    return largest;
+}
+
 int main() {
+//    int main() {
+//        int size;
+//        scanf("%d", &size);
+//        if(size > 10){
+//            printf("Vui long nhap so luong phan tu trong mang so nguyen (so phan tu nho hon hoac bang 10) ");
+//            return 0;
+//        }
+//        else{
+//            int i;
+//            int a[size];
+//
+//            // thuc hien nhap tung phan tu mang
+//            for (i = 0; i < size; i++)
+//            {
+//                printf("Nhap a[%d] = ", i);
+//                scanf("%d", &a[i]);
+//            }
+//
+//            // thuc hien in cac phan tu cua mang ra man hinh
+//            printf("\nMang da nhap \n");
+//            for (i = 0; i < size; i++)
+//            {
+//                printf ("%d \t", a[i]);
+//            }
+//            int luaChon;
+//            printf("Vui long lua chon gia tri mong muon.\n");
+//            scanf("%d", &luaChon);
+//            switch (luaChon) {
+//                case 1:
+//                    printf("So lon nhat trong mang la %d = ", findMaxValue(a));
+//                    break;
+//                case 2:
+//                    printf("So nho nhat trong mang la %d = ", findMinValue(a));
+//                    break;
+//            }
+//        }
+//
+//    int findMinValue(int array[]){
+//        int i, smallest;
+//        smallest= array[0];
+//
+//        for(i = 1; i < array.length; i++) {
+//            if( smallest> array[i] )
+//                smallest= array[i];
+//        }
+//        return smallest;
+//    }
+//
+//
+//    int findMaxValue(int array[]){
+//        int i, largest;
+//        largest = array[0];
+//
+//        for(i = 1; i > array.length; i++) {
+//            if( largest > array[i] )
+//                largest = array[i];
+//        }
+//        return largest;
+//    }
+//        return 0;
+//}
     int size;
     printf("Vui long nhap so luong phan tu trong mang so nguyen (so phan tu nho hon hoac bang 10) ");
     scanf("%d", &size);
@@ -17,36 +99,33 @@ int main() {
         printf("Thong tin phan tu thu %d la %d va %d \n", j + 1, *mangSoNguyen, mangSoNguyen);
         mangSoNguyen++;
     }
-    int findMaxValue;
-    int findMinValue;
-    for (int k = 0; k < size; ++k) {
-        if (findMaxValue < mangSoNguyen[k]);
-        findMaxValue = mangSoNguyen[k];
-    } else if (findMinValue > mangSoNguyen[k]){
-        findMinValue = mangSoNguyen[k];
-    }
+//    int findMaxValue;
+//    int findMinValue;
+//    findMaxValue = mangSoNguyen[0];
+//    findMinValue = mangSoNguyen[0];
+//    for (int k = 0; k < size; ++k) {
+//        if (findMaxValue < mangSoNguyen[k]) {
+//            findMaxValue = mangSoNguyen[k];
+//        }
+//        if (findMinValue > mangSoNguyen[k]) {
+//            findMinValue = mangSoNguyen[k];
+//        }
+//    }
+
     int luaChon;
+    findMaxValue;
+    findMinValue;
     printf("Vui long lua chon gia tri mong muon.\n");
     scanf("%d", &luaChon);
-    switch (luaChon){
+    switch (luaChon) {
         case 1:
-            findMaxValue = mangSoNguyen;
-                printf("So lon nhat trong mang la %d = ", *findMaxValue);
+            mangSoNguyen = findMaxValue;
+            printf("So lon nhat trong mang la %d ", findMaxValue);
             break;
         case 2:
             mangSoNguyen = findMinValue;
-            printf("So nho nhat trong mang la %d = ", *findMinValue);
+            printf("So nho nhat trong mang la %d ", findMinValue);
             break;
     }
-
     return 0;
 }
-
-// Viết chương trình tìm số lớn nhất và nhỏ nhất trong một mảng các số nguyên.
-//    - Yêu cầu người dùng nhập vào số lượng các phần tử trong mảng số nguyên (Yêu cầu số lượng nhỏ hơn hoặc bằng 10)
-//    - Yêu cầu người dùng nhập đủ các phần tử trong mảng và in ra thông tin các phần tử trong mảng.
-//    - Hỏi người dùng muốn in ra số lớn nhất hay nhỏ nhất
-// switch theo lựa chọn của người dùng mà gọi đến một trong hai hàm findMaxValue và findMinValue
-// (Tham số truyền vào là mảng các số nguyên vừa nhập).
-//    - Hàm findMaxValue trả về số nguyên lớn nhất trong mảng, findMinValue trả về số nguyên nhỏ nhất.
-//    - In ra kết quả (Khuyến khích dùng con trỏ)
