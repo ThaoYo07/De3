fe85f8c3c15d09b4c41555b15eeb67b72e069f27
Thao <thaontpth1809022@fpt.edu.vn>
2018-10-12 17:05:57 +0700

    BaiCuoi

--- main.c
+++ main.c
@@ -22,67 +22,6 @@ int findMaxValue(int array[]) {
 }
 
 int main() {
-//    int main() {
-//        int size;
-//        scanf("%d", &size);
-//        if(size > 10){
-//            printf("Vui long nhap so luong phan tu trong mang so nguyen (so phan tu nho hon hoac bang 10) ");
-//            return 0;
-//        }
-//        else{
-//            int i;
-//            int a[size];
-//
-//            // thuc hien nhap tung phan tu mang
-//            for (i = 0; i < size; i++)
-//            {
-//                printf("Nhap a[%d] = ", i);
-//                scanf("%d", &a[i]);
-//            }
-//
-//            // thuc hien in cac phan tu cua mang ra man hinh
-//            printf("\nMang da nhap \n");
-//            for (i = 0; i < size; i++)
-//            {
-//                printf ("%d \t", a[i]);
-//            }
-//            int luaChon;
-//            printf("Vui long lua chon gia tri mong muon.\n");
-//            scanf("%d", &luaChon);
-//            switch (luaChon) {
-//                case 1:
-//                    printf("So lon nhat trong mang la %d = ", findMaxValue(a));
-//                    break;
-//                case 2:
-//                    printf("So nho nhat trong mang la %d = ", findMinValue(a));
-//                    break;
-//            }
-//        }
-//
-//    int findMinValue(int array[]){
-//        int i, smallest;
-//        smallest= array[0];
-//
-//        for(i = 1; i < array.length; i++) {
-//            if( smallest> array[i] )
-//                smallest= array[i];
-//        }
-//        return smallest;
-//    }
-//
-//
-//    int findMaxValue(int array[]){
-//        int i, largest;
-//        largest = array[0];
-//
-//        for(i = 1; i > array.length; i++) {
-//            if( largest > array[i] )
-//                largest = array[i];
-//        }
-//        return largest;
-//    }
-//        return 0;
-//}
     int size;
     printf("Vui long nhap so luong phan tu trong mang so nguyen (so phan tu nho hon hoac bang 10) ");
     scanf("%d", &size);
@@ -99,20 +38,7 @@ int main() {
         printf("Thong tin phan tu thu %d la %d va %d \n", j + 1, *mangSoNguyen, mangSoNguyen);
         mangSoNguyen++;
     }
-//    int findMaxValue;
-//    int findMinValue;
-//    findMaxValue = mangSoNguyen[0];
-//    findMinValue = mangSoNguyen[0];
-//    for (int k = 0; k < size; ++k) {
-//        if (findMaxValue < mangSoNguyen[k]) {
-//            findMaxValue = mangSoNguyen[k];
-//        }
-//        if (findMinValue > mangSoNguyen[k]) {
-//            findMinValue = mangSoNguyen[k];
-//        }
-//    }
-
-    int luaChon;
+   int luaChon;
     findMaxValue;
     findMinValue;
     printf("Vui long lua chon gia tri mong muon.\n");
